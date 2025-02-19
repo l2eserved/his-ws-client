f8769daf1bb3989821beccae152a8655a8b919f7
l2eserved <gradons2536@gmail.com>
2025-02-19 15:13:41 +0700

    Initial commit

--- firebase_setting.js
+++ firebase_setting.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCdYavvULjAoZzlGwH2M0apdCnyhhbB9Us",
-    authDomain: "tgh-auth.firebaseapp.com",
-    projectId: "tgh-auth",
-    storageBucket: "tgh-auth.firebasestorage.app",
-    messagingSenderId: "697488607080",
-    appId: "1:697488607080:web:d755283d1aa2db0ffb78dd"
+
 };
+
+
+
+
 
 export { firebaseConfig };
